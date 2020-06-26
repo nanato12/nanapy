7d8483e93455b91a0b8f76ebbc43004faf45fb5e
nanato12 <admin@nanato12.info>
2020-06-26 19:19:13 +0900

    Update service.py
    ・プロキシ設定の追加
    
    ・request系メソッドをまとめる

--- nanapy/service.py
+++ nanapy/service.py
@@ -1,5 +1,7 @@
 import requests
 
+from requests.exceptions import Timeout
+
 from .func import Func
 from .config import Config
 from .error import (
@@ -15,7 +17,8 @@ from .error import (
     ApplauseError,
     CommentError,
     CommunityJoinError,
-    CommunityLeaveError
+    CommunityLeaveError,
+    NanapyError
 )
 
 class Service(Config):
@@ -23,22 +26,33 @@ class Service(Config):
     email = None
     password = None
     token = None
+    proxy = None
     device_id = None
     appsflyer_id = None
     session = None
 
-    def __init__(self, email=None, password=None, token=None):
+    def __init__(self, email, password, name, token, create, proxy):
         self.initialize_session()
+
+        if proxy:
+            self.proxy = {'https': proxy}
 
         self.email = email
         self.password = password
 
-        if token:
+        if create:
+            self.create_account(name=name)
+        elif token:
             self.set_token(token)
-        elif self.email is not None and self.password is not None:
+        elif email and password:
             self.login()
         else:
-            self.create_account(email=email, password=password)
+            raise NanapyError(
+                '\n\nCreate or Login failed.\n' + \
+                'If you want create account: Nana(create=True)\n' + \
+                'If you want login: Nana(email, password) or ' + \
+                'Nana(token=\'XXXXXXXXXX\')\n'
+            )
 
     def initialize_session(self):
         self.session = requests.session()
@@ -46,6 +60,40 @@ class Service(Config):
         self.appsflyer_id = Func.generate_appsflyer_id()
         self.HEADERS['x-device-identifier'] = self.device_id
         self.HEADERS['x-appsflyer-identifier'] = self.appsflyer_id
+
+    def get_request(self, url):
+        try:
+            return self.session.get(
+                url=url,
+                headers=self.HEADERS,
+                proxies=self.proxy,
+                timeout=5.0
+            )
+        except Timeout:
+            raise TimeoutError('connection timeout.')
+
+    def post_request(self, url, json=None):
+        try:
+            return self.session.post(
+                url=url,
+                headers=self.HEADERS,
+                json=json,
+                proxies=self.proxy,
+                timeout=5.0
+            )
+        except Timeout:
+            raise TimeoutError('connection timeout.')
+
+    def delete_request(self, url):
+        try:
+            return self.session.delete(
+                url=url,
+                headers=self.HEADERS,
+                proxies=self.proxy,
+                timeout=5.0
+            )
+        except Timeout:
+            raise TimeoutError('connection timeout.')
 
     def login(self):
         url = self.HOST_DOMAIN + self.LEGACY_VERSION + self.LOGIN_PATH
@@ -55,34 +103,31 @@ class Service(Config):
             'password': self.password,
             'type': 'nana'
         }
-        res = self.session.post(
-            url=url,
-            headers=self.HEADERS,
-            json=params
-        )
+
+        res = self.post_request(url, json=params)
         resource = res.json()
         if res.status_code != 200:
             raise LoginError(resource['data']['message'])
         self.set_token(resource['data']['token'])
 
-    def create_account(self, name=None, email=None, password=None):
-        self.name = name if name else Func.random_string(6)
-        self.email = email if email else f'{self.name}@email.com'
-        self.password = password if password else Func.random_hex(6)
+    def create_account(self, name=None):
+        if name is None:
+            name = Func.random_string(6)
+        if self.email is None:
+            self.email = f'{name}@email.com'
+        if self.password is None:
+            self.password = Func.random_hex(6)
 
         url = self.HOST_DOMAIN + self.LEGACY_VERSION + self.SIGNUP_PATH
         params = {
             'device_id': self.device_id,
-            'screen_name': self.name,
+            'screen_name': name,
             'email': self.email,
             'password': self.password,
             'type': 'nana'
         }
-        res = self.session.post(
-            url=url,
-            headers=self.HEADERS,
-            json=params
-        )
+
+        res = self.post_request(url, json=params)
         resource = res.json()
         if res.status_code != 200:
             raise SignupError(resource['data']['message'])
@@ -94,10 +139,8 @@ class Service(Config):
 
     def get_my_info(self):
         url = self.HOST_DOMAIN + self.LATEST_VERSION + self.MYPAGE_PATH
-        res = self.session.get(
-            url=url,
-            headers=self.HEADERS
-        )
+
+        res = self.get_request(url)
         resource = res.json()
         if res.status_code != 200:
             raise NotFoundMyData(resource['data']['message'])
@@ -106,10 +149,8 @@ class Service(Config):
     def get_user(self, user_id):
         url = self.HOST_DOMAIN + self.LEGACY_VERSION + \
             self.USER_PATH.format(user_id=user_id)
-        res = self.session.get(
-            url=url,
-            headers=self.HEADERS
-        )
+
+        res = self.get_request(url)
         if res.status_code != 200:
             raise NotFoundUser(f'{user_id} is not found.')
         return res.json()
@@ -117,10 +158,8 @@ class Service(Config):
     def get_post(self, post_id):
         url = self.HOST_DOMAIN + self.LEGACY_VERSION + \
             self.POST_PATH.format(post_id=post_id)
-        res = self.session.get(
-            url=url,
-            headers=self.HEADERS
-        )
+
+        res = self.get_request(url)
         if res.status_code != 200:
             raise NotFoundUser(f'{post_id} is not found.')
         return res.json()
@@ -128,10 +167,8 @@ class Service(Config):
     def get_community(self, community_id):
         url = self.HOST_DOMAIN + self.LEGACY_VERSION + \
             self.COMMUNITY_PATH.format(community_id=community_id)
-        res = self.session.get(
-            url=url,
-            headers=self.HEADERS
-        )
+
+        res = self.get_request(url)
         if res.status_code != 200:
             raise NotFoundCommunity(f'{community_id} is not found.')
         return res.json()
@@ -140,10 +177,7 @@ class Service(Config):
         url = self.HOST_DOMAIN + self.LATEST_VERSION + \
             self.USER_FOLLOW_PATH.format(user_id=user_id)
 
-        res = self.session.post(
-            url=url,
-            headers=self.HEADERS
-        )
+        res = self.post_request(url)
         if res.status_code != 200:
             raise FollowError('failed.')
         return res.json()
@@ -152,10 +186,7 @@ class Service(Config):
         url = self.HOST_DOMAIN + self.LATEST_VERSION + \
             self.USER_FOLLOW_PATH.format(user_id=user_id)
 
-        res = self.session.delete(
-            url=url,
-            headers=self.HEADERS
-        )
+        res = self.delete_request(url)
         resource = res.json()
         if res.status_code != 200:
             raise FollowError('failed.')
@@ -165,10 +196,7 @@ class Service(Config):
         url = self.HOST_DOMAIN + self.LEGACY_VERSION + \
             self.POST_PLAY_PATH.format(post_id=post_id)
 
-        res = self.session.post(
-            url=url,
-            headers=self.HEADERS
-        )
+        res = self.post_request(url)
         if res.status_code != 200:
             raise PlayError('failed.')
 
@@ -176,10 +204,7 @@ class Service(Config):
         url = self.HOST_DOMAIN + self.LEGACY_VERSION + \
             self.POST_APPLAUSE_PATH.format(post_id=post_id)
 
-        res = self.session.post(
-            url=url,
-            headers=self.HEADERS
-        )
+        res = self.post_request(url)
         if res.status_code != 200:
             raise ApplauseError('failed.')
 
@@ -187,10 +212,7 @@ class Service(Config):
         url = self.HOST_DOMAIN + self.LEGACY_VERSION + \
             self.POST_APPLAUSE_PATH.format(post_id=post_id)
 
-        res = self.session.delete(
-            url=url,
-            headers=self.HEADERS
-        )
+        res = self.delete_request(url)
         if res.status_code != 200:
             raise ApplauseError('failed.')
 
@@ -198,11 +220,7 @@ class Service(Config):
         url = self.HOST_DOMAIN + self.LEGACY_VERSION + \
             self.POST_COMMENT_PATH.format(post_id=post_id)
 
-        res = self.session.post(
-            url=url,
-            headers=self.HEADERS,
-            json={'body': comment}
-        )
+        res = self.post_request(url, json={'body': comment})
         if res.status_code != 200:
             raise CommentError('failed.')
 
@@ -210,10 +228,7 @@ class Service(Config):
         url = self.HOST_DOMAIN + self.LEGACY_VERSION + \
             self.COMMUNITY_JOIN_PATH.format(community_id=community_id)
 
-        res = self.session.post(
-            url=url,
-            headers=self.HEADERS
-        )
+        res = self.post_request(url)
         if res.status_code != 200:
             raise CommunityJoinError('failed.')
 
@@ -221,20 +236,14 @@ class Service(Config):
         url = self.HOST_DOMAIN + self.LEGACY_VERSION + \
             self.COMMUNITY_JOIN_PATH.format(community_id=community_id)
 
-        res = self.session.delete(
-            url=url,
-            headers=self.HEADERS
-        )
+        res = self.delete_request(url)
         if res.status_code != 200:
             raise CommunityLeaveError('failed.')
 
     def logout(self):
         url = self.HOST_DOMAIN + self.LEGACY_VERSION + self.LOGOUT_PATH
 
-        res = self.session.post(
-            url=url,
-            headers=self.HEADERS
-        )
+        res = self.post_request(url)
         resource = res.json()
         if res.status_code != 200:
             raise LogoutError('failed.')
